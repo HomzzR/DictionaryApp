b9bdc9a1b8e0b67dc33e348b4c374ed5c7f5a58b
HomzzR <homzzr@yandex.ru>
2023-07-02 13:01:47 +0300

    Добавил таблицу

--- DictionaryApp/ViewController.swift
+++ DictionaryApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let words = ["Apple", "Pear", "Watermelon", "Carrot", "Potato", "Tomato"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        
+        if let reusedCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reusedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        
+        cell.textLabel?.text = words[indexPath.row]
+        
+        return cell
+    }
 }
 
