bc7f0263e6950fd0527689c324e27bed9e090f64
HomzzR <homzzr@yandex.ru>
2023-07-02 17:11:27 +0300

    Добавил алерт

--- DictionaryApp/ViewController.swift
+++ DictionaryApp/ViewController.swift
@@ -9,17 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let words = ["Apple", "Pear", "Watermelon", "Carrot", "Potato", "Tomato"]
+    let words = [
+        ["Apple", "Pear", "Watermelon"],
+        ["Carrot", "Pickle", "Potato", "Tomato"]
+    ]
+    
+    let headers = ["Fruits", "Vegetables"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.sectionHeaderHeight = 32
     }
 }
 
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let vegOrFruitArray = words[section]
+        return vegOrFruitArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,10 +45,32 @@ extension ViewController: UITableViewDataSource {
         } else {
             cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         }
-        
-        cell.textLabel?.text = words[indexPath.row]
-        
+        var content = cell.defaultContentConfiguration()
+        content.text = words[indexPath.section][indexPath.row]
+        cell.contentConfiguration = content
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
     }
 }
 
+extension UIViewController: UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let words = [
+            ["Apple", "Pear", "Watermelon"],
+            ["Carrot", "Pickle", "Potato", "Tomato"]
+        ]
+        let alert = UIAlertController(title: nil,
+                                      message: "Вы нажали на: \(words[indexPath.section][indexPath.row])",
+                                      preferredStyle: .alert)                         
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            alert.dismiss(animated: true)
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
